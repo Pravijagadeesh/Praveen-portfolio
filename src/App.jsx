38c2460ab797d18f7baed410f71cad6ddc810c8a
Pravijagadeesh <jpraveen577@gmail.com>
2024-12-04 11:55:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import Portfolio from "./Components/Portf";
+import Skills from "./Components/Skills";
+import Contact from "./Components/Contact";
+
+
+export default function App() {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <Skills />
+    <Portfolio />
+    
+    <Contact />
+  
+    
+    
+    </>
+  )
+}
